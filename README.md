b1fc44540e9bf800edd0ad810c30caffe642bb74
yyc001 <1594088331@qq.com>
2023-04-06 19:45:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+# 数据库大作业
+
+因为 Oracle 安装太麻烦了，于是用了 MySQL
+
+## install
+
+安装 Docker
+```shell
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo service docker start
+```
+启动数据库
+```shell
+sudo docker compose up -d
+```
+初始化数据库
+```shell
+sudo docker exec flask-mysql "mysql -uroot -pexample < /db/setup.sql"
+```
+配置 python 虚拟环境
+
+如果已经安装了 conda ，可以用 conda 配置虚拟环境：
+```shell
+conda create -y -n db_project python=3.9
+conda activate db_project
+pip install -r requirements.txt
+```
+如果没有安装 conda ，也可以通过如下方法配置虚拟环境：
+```shell
+python -m venv env
+source ./env/bin/activate
+pip install -r requirements.txt
+```
+
+## start
+
+```shell
+python -m flask run
+```
+如果刚安装完能成功运行，一段时间后运行不了，请检查一下 docker 和 python venv：
+```shell
+sudo docker ps -a
+which python
+```
+用 `sudo service docker restart` 可以重启 docker。
--- db/setup.sql
+++ db/setup.sql
@@ -1,20 +1,25 @@
-drop user if exists 'student1';
-drop database `student1`;
-create user 'student1'@'%' identified by '123456';
-CREATE DATABASE if not exists `student1`;
-grant all on student1.* to 'student1' with grant option;
-
-drop user if exists 'student2';
-CREATE DATABASE if not exists `student2`;
-create user 'student2'@'%' identified by '123456';
-drop database `student2`;
-grant all on student2.* to 'student2' with grant option;
-
-flush privileges;
-
 create database if not exists `pub`;
 create table pub.sc (
     sid varchar(12) not null,
     cid varchar(12) not null,
     score numeric(6,1)
 );
+
+insert into pub.sc values ('123456789011', '098765432100', 90);
+
+
+drop user if exists 'student1';
+drop database `student1`;
+create user 'student1'@'%' identified by '123456';
+CREATE DATABASE if not exists `student1`;
+grant all on student1.* to 'student1' with grant option;
+grant select on pub.* to 'student1';
+
+drop user if exists 'student2';
+CREATE DATABASE if not exists `student2`;
+create user 'student2'@'%' identified by '123456';
+drop database `student2`;
+grant all on student2.* to 'student2' with grant option;
+grant select on pub.* to 'student2';
+
+flush privileges;
