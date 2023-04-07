b783369e26f0700d4ac9448188a9991c2f6e97d8
yyc001 <1594088331@qq.com>
2023-04-07 16:42:06 +0800

    fix setup

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ sudo sh get-docker.sh
 sudo service docker start
 # start mysql daemon
 sudo docker compose up -d
-# initialize database
-sudo docker exec flask-mysql /bin/sh -c "mysql -uroot -pexample < /db/setup.sql"
 # create virtualenv
 python -m venv env
 source ./env/bin/activate
 pip install -r requirements.txt
+# initialize database
+sudo docker exec flask-mysql /bin/sh -c "mysql -uroot -pexample < /db/setup.sql"
