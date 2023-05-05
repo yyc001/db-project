0400764a6b50e85fa5cc8d9e298ae6a5f7908ad1
Chen <2560570216@qq.com>
2023-05-05 16:34:07 +0800

    update sql

--- db/setup.sql
+++ db/setup.sql
@@ -23,4 +23,30 @@ create database `student2`;
 grant all on student2.* to 'student2' with grant option;
 grant select on pub.* to 'student2';
 
-flush privileges;
+
+
+drop database if exists 'answer';
+create database 'answer';
+create table answer.0_0(
+    sid varchar(12) not null,
+    name varchar(12) not null
+)
+insert into answer.0_0 values('123', 'yyc')
+insert into answer.0_0 values('125', 'gjc')
+insert into answer.0_0 values('127', 'cz')
+
+drop database if exists 'admin';
+create database 'admin';
+create table admin.record(
+    sid varchar(12) not null,
+    submission_time datetime not null,
+    test_id varchar(12) not null,
+    result varchar(30) not null
+)
+
+create table admin.test_table(
+    test_id varchar(12) not null,
+    test_name varchar(12) not null,
+    test_desc varchar(100) not null 
+)
+
